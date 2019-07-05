963219890f910a31fba1f46807d4976d12db787c
Sebastian Badea <sebastian.badea@gmail.com>
2019-07-05 09:52:06 +0100

    changed the message

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a change from the pull branch");
+        }
+    }
+}
